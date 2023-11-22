8f1bbb339284e88ac5d07962696ec92bbc0d39f9
Quentin Anthony <qganthony@yahoo.com>
2023-11-22 11:25:54 -0800

    Interview

--- mingpt/model.py
+++ mingpt/model.py
@@ -46,80 +46,32 @@ def causal_self_attention(x, config, dropout):
     explicit implementation here to show that there is nothing too scary here.
     """
     assert config.n_embd % config.n_head == 0
-    T, E = x.shape # Tokens, Embeddings
-    LL = partial(Linear, output_size=config.n_embd)
-    
-    # causal mask to ensure that attention is only applied to the left in the input sequence
-    mask = jnp.tril(jnp.ones((config.block_size,config.block_size)))
-    
-    # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-    k = LL(name='linear_k')(x)
-    q = LL(name='linear_q')(x)
-    v = LL(name='linear_v')(x)
-    # nh - number of heads (n_head), hs - head size
-    # (T, E=nh*hs) -> (T, nh, hs) -> (nh, T, hs)
-    hs = E // config.n_head
-    resh = lambda z: z.reshape(T, config.n_head, hs).swapaxes(0,1) 
-    k,q,v = map(resh, (k,q,v))
-    
-    # causal self-attention; Self-attend: (nh, T, hs) x (nh, hs, T) -> (nh, T, T)
-    att = q @ k.swapaxes(1,2) 
-    att = att / math.sqrt(hs)
-    att = jnp.where(mask[:T,:T], att, float('-inf'))
-    
-    att = jax.nn.softmax(att, axis=-1)
-    att = dropout(config.attn_pdrop, att)
-    # (nh, T, T) x (nh, T, hs) -> (nh, T, hs)
-    y = att @ v 
-    # (nh, T, hs) -> (T, nh, hs) -> (T, E) - re-assemble all head outputs
-    y = y.swapaxes(0,1).reshape(T, E) 
+    #TODO
 
-    # output projection
-    y = LL(name='linear_proj')(y)
-    y = dropout(config.resid_pdrop, y)
+
     return y
 
 def block(x, config, dropout):
     """ an unassuming Transformer block """
-    ln1 = LayerNorm(name='layer_norm1')
-    ln2 = LayerNorm(name='layer_norm2')
-                
-    mlp = hk.Sequential([
-        Linear(4 * config.n_embd, name='linear_mlp1'),
-        jax.nn.gelu,
-        Linear(config.n_embd, name='linear_mlp2'),
-    ])
-    x = x + causal_self_attention(ln1(x), config, dropout)
-    mlp = mlp(ln2(x))
-    mlp = dropout(config.resid_pdrop, mlp)
-    return x + mlp
+    #TODO
 
 def gpt(x, config, is_training):
     """  the full GPT language model, with a context size of block_size """
     dropout = Dropout(is_training)
     # input embedding stem
-    tok_emb = hk.Embed(config.vocab_size, config.n_embd, w_init=normal_init)
-    pos_emb = hk.get_parameter("embeddings", shape=[config.block_size, config.n_embd], 
-                                   dtype=jnp.float32, init=jnp.zeros)
+
         
     # decoder head
-    ln_f = LayerNorm(name='layer_norm_f')
-    head = Linear(config.vocab_size, with_bias=False, name='linear_head')
-    
-    t = x.shape[0]
-    assert t <= config.block_size, "Cannot forward, model block size is exhausted."
+
 
     # forward the GPT model
-    token_embeddings = tok_emb(x) # each index maps to a (learnable) vector
-    position_embeddings = pos_emb[:t, :] # each position maps to a (learnable) vector
-    x = token_embeddings + position_embeddings
-    x = dropout(config.embd_pdrop, x)
+
+    # each index maps to a (learnable) vector
+
+    # each position maps to a (learnable) vector
+
     # transformer
-    blk_fn = partial(block, config=config, dropout=dropout)
-    blocks = hk.Sequential([blk_fn for _ in range(config.n_layer)])
-    x = blocks(x)
-    x = ln_f(x)
-    return head(x)
+
 
 def cross_entropy(logits, targets):
     one_hot = jax.nn.one_hot(targets, logits.shape[-1])
